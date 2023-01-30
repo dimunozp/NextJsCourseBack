1dc71fa25e76548629244ed5e19a7492a4ff0b89
Diego Munoz <diego.munoz@kushkipagos.com>
2023-01-30 14:49:37 -0500

    Create Is Owner Policy For Events

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -7,6 +7,63 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Create event with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.entityService.create("api::event.event", {
+        data: data,
+        files,
+      });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.entityService.create("api::event.event", {
+        data: { ...ctx.request.body },
+      });
+    }
+    return await this.sanitizeOutput(entity, ctx);
+  },
+  // Update a user event
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    console.log(0, id, ctx.state.user);
+    const [events] = await strapi.entityService.findMany("api::event.event", {
+      filters: { id: ctx.params.id, user: ctx.state.user },
+    });
+    if (!events) {
+      return ctx.unauthorized("You can't update this entry");
+    }
+    if (ctx.is("multipart")) {
+      console.log(1);
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.entityService.update("api::event.event", id, {
+        data: data,
+        files,
+      });
+    } else {
+      console.log(2);
+      entity = await strapi.entityService.update("api::event.event", id, {
+        data: { ...ctx.request.body },
+      });
+    }
+    return await this.sanitizeOutput(entity, ctx);
+  },
+  // Delete a user event
+  async delete(ctx) {
+    const { id } = ctx.params;
+    const [events] = await strapi.entityService.findMany("api::event.event", {
+      filters: { id: ctx.params.id, user: ctx.state.user },
+    });
+    if (!events) {
+      return ctx.unauthorized("You can't update this entry");
+    }
+    const entity = await strapi.entityService.update("api::event.event", id);
+    return await this.sanitizeOutput(entity, ctx);
+  },
+  // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
 
@@ -18,10 +75,9 @@ module.exports = createCoreController("api::event.event", ({ strapi }) => ({
       ]);
     }
 
-    const data = await strapi
-      .entityService.findMany("api::event.event", {
-        filters: { user: user },
-      });
+    const data = await strapi.entityService.findMany("api::event.event", {
+      filters: { user: user },
+    });
 
     if (!data) {
       return ctx.notFound();
